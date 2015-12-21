ce2c50cacb6a73170b8a17d16f8cc72d396a310a
Spencer Norman <spencern@gmail.com>
2015-12-21 14:55:49 -0700

    Ignore rental inventory in reaction-inventory

--- common/hooks.js
+++ common/hooks.js
@@ -9,27 +9,27 @@
  * After cart update
  */
 
-ReactionCore.Collections.Cart.after.update(function (userId, cart, fieldNames, modifier) {
-  // if we're adding a new product or variant to the cart
-  if (modifier.$addToSet) {
-    if (modifier.$addToSet.items) {
-      ReactionCore.Log.info("after cart update, call inventory/addReserve");
-      Meteor.call("inventory/addReserve", cart.items);
-    }
-  }
-  // or we're adding more quuantity
-  if (modifier.$inc) {
-    ReactionCore.Log.info("after variant increment, call inventory/addReserve");
-    Meteor.call("inventory/addReserve", cart.items);
-  }
-  // removing  cart items, clear inventory reserve
-  if (modifier.$pull) {
-    if (modifier.$pull.items) {
-      ReactionCore.Log.info("remove cart items, call inventory/clearReserve");
-      Meteor.call("inventory/clearReserve", cart.items);
-    }
-  }
-});
+// ReactionCore.Collections.Cart.after.update(function (userId, cart, fieldNames, modifier) {
+//   // if we're adding a new product or variant to the cart
+//   if (modifier.$addToSet) {
+//     if (modifier.$addToSet.items) {
+//       ReactionCore.Log.info("after cart update, call inventory/addReserve");
+//       Meteor.call("inventory/addReserve", cart.items);
+//     }
+//   }
+//   // or we're adding more quuantity
+//   if (modifier.$inc) {
+//     ReactionCore.Log.info("after variant increment, call inventory/addReserve");
+//     Meteor.call("inventory/addReserve", cart.items);
+//   }
+//   // removing  cart items, clear inventory reserve
+//   if (modifier.$pull) {
+//     if (modifier.$pull.items) {
+//       ReactionCore.Log.info("remove cart items, call inventory/clearReserve");
+//       Meteor.call("inventory/clearReserve", cart.items);
+//     }
+//   }
+// });
 
 
 //
@@ -37,6 +37,9 @@ ReactionCore.Collections.Cart.after.update(function (userId, cart, fieldNames, m
 //
 ReactionCore.Collections.Products.before.update(function (userId, product, fieldNames, modifier) {
   // removing  items
+  if (product.type === "rental") {
+    return false;
+  }
   if (modifier.$pull) {
     if (modifier.$pull.variants) {
       let variantItem = {
@@ -54,6 +57,9 @@ ReactionCore.Collections.Products.before.update(function (userId, product, field
 //
 ReactionCore.Collections.Products.after.update(function (userId, product,
   fieldNames, modifier) {
+  if (product.type === "rental") {
+    return false;
+  }
   if (modifier.$push) { // if we're adding a new product or variant
     Meteor.call("inventory/register", product);
   }
